7f3659a77988ec2cc0a75926647ba1d1f7fdaaca
Lay <thwelay13@gmail.com>
2023-03-18 00:35:55 +0630

    Admin Dashboard

---
+++ Admin/View/dashboard_managePage_Services.php
@@ -0,0 +1,86 @@
+<?php
+include "./resources/shared/dashboard.php";
+?>
+<link rel="stylesheet" href="./resources/css/dashboard_managePage.css">
+<link rel="stylesheet" href="./resources/css/dashboard_managePage_Services.css">
+
+</head>
+
+<body>
+
+    <?php
+    include "./resources/shared/dashboard_managePage.php";
+    ?>
+    <form>
+
+        <div class="d-flex flex-column justify-content-center align-items-center"> 
+
+            <div class="my-3 mt-5"> 
+
+               <div class="box">
+                    <label for="textbox1">Text Box 1</label>
+                    <input type="text" id="textbox1">
+                </div>
+                <div class="box">
+                    <label for="icon1">Icon 1</label>
+                    <input type="text" id="icon1">
+                </div>
+                <div class="box">
+                    <label for="textbox1">Text Box 2</label>
+                    <input type="text" id="textbox1">
+                </div>
+                <div class="box">
+                    <label for="icon1">Icon 2</label>
+                    <input type="text" id="icon2">
+                </div>
+                <div class="box">
+                    <label for="textbox1">Text Box 3</label>
+                    <input type="text" id="textbox1">
+                </div>
+                <div class="box">
+                    <label for="icon1">Icon 3</label>
+                    <input type="text" id="icon3">
+                </div>
+                <div class="box">
+                    <label for="textbox1">Text Box 4</label>
+                    <input type="text" id="textbox1">
+                </div>
+                <div class="box">
+                    <label for="icon1">Icon 4</label>
+                    <input type="text" id="icon4">
+                </div>
+                <div class="box">
+                    <label for="textbox1">Text Box 5</label>
+                    <input type="text" id="textbox1">
+                </div>
+                <div class="box">
+                    <label for="icon1">Icon 5</label>
+                    <input type="text" id="icon5">
+                </div>
+                <div class="box">
+                    <label for="textbox1">Text Box 6</label>
+                    <input type="text" id="textbox1">
+                </div>
+                <div class="box">
+                    <label for="icon1">Icon 6</label>
+                    <input type="text" id="icon6">
+                </div>
+
+            </div>
+
+
+      
+        </div>
+        <div class="saveBtn mb-5">
+            <button>Save Change <iconify-icon icon="material-symbols:save"></iconify-icon></button>
+        </div>
+
+        </form>
+    <!----------  Ending Tab ------>
+    </div>
+    </div>
+    <script src="./resources/js/dashboard.js"></script>
+    <script src="./resources/js/dashboard_managePage.js"></script>
+</body>
+
+</html>
