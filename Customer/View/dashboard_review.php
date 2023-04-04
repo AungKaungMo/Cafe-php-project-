e6649c0560f81a47070ca5e0cf7ed00dd8b2b601
EiPwint <eipwintphyo1@gmail.com>
2023-04-04 10:22:58 +0630

    commit

--- Customer/View/dashboard_review.php
+++ Customer/View/dashboard_review.php
@@ -1,5 +1,6 @@
 <?php
 include "./resources/shared/dashboard.php";
+include "../Controller/selectreviewController.php";
 ?>
 <link rel="stylesheet" href="./resources/css/dashboard_review.css">
 </head>
@@ -10,7 +11,14 @@ include "./resources/shared/dashboard.php";
             <div>
                 <div class="commentBox">
                     <label class="fs-5">Your Comment</label><br>
-                    <textarea class="p-2 rounded-3" placeholder="Write Something" name="com"></textarea>
+                    <?php
+                    if ($mode == 1) { ?>
+                        <textarea class="p-2 rounded-3" placeholder="Write Something" name="com"></textarea>
+                    <?php } else { ?>
+                        <textarea class="p-2 rounded-3" placeholder="Write Something" name="com"><?= $Result[0]["comment"] ?></textarea>
+                    <?php    }
+
+                    ?>
                 </div>
                 <div class="mt-4 rating">
                     <p class="fs-5 mb-1">Rating</p>
@@ -18,7 +26,6 @@ include "./resources/shared/dashboard.php";
                         <label for="s1">
                             <iconify-icon icon="ic:round-star" class="ratestar" id="s1"></iconify-icon>
                         </label>
-
                         <label for="s2">
                             <iconify-icon icon="ic:round-star" class="ratestar" id="s2"></iconify-icon>
                         </label>
@@ -37,6 +44,7 @@ include "./resources/shared/dashboard.php";
             </div>
         </div>
         <div class="saveBtn ">
+            <input type="hidden" name="select" id="" value="<?= $mode ?>">
             <button name="save">Save Change <iconify-icon icon="material-symbols:save"></iconify-icon></button>
         </div>
     </form>
