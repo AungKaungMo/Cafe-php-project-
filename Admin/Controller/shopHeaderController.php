10e60865c947265930e32d1d78f402b4ff080ae6
Lay <thwelay13@gmail.com>
2023-04-04 18:55:46 +0630

    no message

--- Admin/Controller/shopHeaderController.php
+++ Admin/Controller/shopHeaderController.php
@@ -1,33 +1,34 @@
 <?php
-
+ini_set("display_errors", "1");
 session_start();
-if (isset($_POST["update"])) {
+if (isset($_POST["submit"])) {
     //     echo "<pre>";
     //     print_r($_FILES["profile"]);
     // }
     include "../Model/dbconnection.php";
     $headerTitle = $_POST['headerTitle'];
     $headerText = $_POST['headerText'];  
-
-
     $file = $_FILES['headerImage']['name'];
+    $db = new DBConnection();
+    $pdo = $db->connect();
 
     if ($file != "") {
 
         $location = $_FILES['headerImage']['tmp_name'];
-        $extension = pathinfo($file)['extension'];
-if (move_uploaded_file($location, "../stroages/shopheader/" . $file . "." . $extension)) {
+
+if (move_uploaded_file($location, "../../Storages/shopheader/" . $file )) {
             //echo 'File is successfully uploaded.';
 
             $sql = $pdo->prepare(
                 "
             UPDATE m_shopHeader SET 
             header_title = :headerTitle,
-            header_text = :headerText
-            WHERE id = :id
+            header_text = :headerText,
+            header_image = :headerImage
+            WHERE id = 1
             "
             );
-            $sql->bindValue(":profile", "../stroages/shopheader/" . $file . "." . $extension);
+            $sql->bindValue(":headerImage", "../../Storages/" . $file );
         }
     } else {
         //echo 'There was some error moving the file to upload directory.';
@@ -36,14 +37,14 @@ if (move_uploaded_file($location, "../stroages/shopheader/" . $file . "." . $ext
             UPDATE m_shopHeader SET 
             header_title = :headerTitle,
             header_text = :headerText
-            WHERE id = :id
+            WHERE id = 1
             "
         );
     }
     $sql->bindValue(":headerTitle", $headerTitle);
     $sql->bindValue(":headerText", $headerText);
     $sql->execute();
-    header("Location: ./View/dashbaord_managePage_shopHeader.php");
+    header("Location: ../View/dashboard_managePage_shopHeader.php");
 } else {
     echo "NOT";
 }
--- ShopOwner/View/dashboard_setting_blog.php
+++ ShopOwner/View/dashboard_setting_blog.php
@@ -18,64 +18,77 @@ include "./resources/shared/dashboard.php";
 
                 <div class="box">
                     <label for="maintitle">Main Title</label>
-                    <input type="text" id="maintitle">
+                    <input type="text" id="maintitle" name="mTitle">
                 </div>
                 <div class="box">
                     <label for="firsttitle">First Title</label>
-                    <input type="text" id="firsttitle">
+                    <input type="text" id="firsttitle" name="fTitle">
                 </div>
                 <div class="box">
                     <label for="secondtitle">Second Title</label>
-                    <input type="text" id="secondtitle">
+                    <input type="text" id="secondtitle" name="sTitle">
                 </div>
                 <div class="box">
                     <label for="thirdtitle">Third Title</label>
-                    <input type="text" id="thirdtitle">
+                    <input type="text" id="thirdtitle" name="tTitle">
                 </div>
                 <div class="box">
                     <label for="firstparagraph">First Paragraph</label>
-                    <textarea id="firstparagraph"></textarea>
+                    <textarea id="firstparagraph" name="fPara"></textarea>
                 </div>
                 <div class="box">
                     <label for="secondparagraph">Second Paragraph</label>
-                    <textarea id="secondparagraph"></textarea>
+                    <textarea id="secondparagraph" name="sPara"></textarea>
                 </div>
                 <div class="box">
                     <label for="thirdparagraph">Third Paragraph</label>
-                    <textarea id="thirdparagraph"></textarea>
+                    <textarea id="thirdparagraph" name="tPara"></textarea>
                 </div>
-                <div class="box">
+                <!-- <div class="box">
                     <label for="date">Second Title</label>
-                    <input type="date" id="date">
-                </div>
-                <div class="box">
+                    <input type="date" id="date" name="date">
+                </div> -->
+                <!-- <div class="box">
                     <label for="blogpicture">Blog Picture</label>
                     <input type="file" id="blogpicture" accept="image/png, image/jpeg">
+                </div> -->
+                <div class="box">
+                    <div class="m-auto aboutImg"> Blog Image
+                        <img src=""  id="acceptImage">
+                        <iconify-icon icon="material-symbols:cloud-upload" class="aboutIcon" width="30" height="30" id="upload"></iconify-icon>
+
+                    </div>
+                    <input type="file" hidden id="picture" accept="image/png, image/jpeg" name="blogImage">
+                </div>
+
+                <div class="box">
+                    <label for="authorname">Aurthor Name</label>
+                    <input type="text" id="authorname" name="auName">
                 </div>
                 <div class="box">
-                    <label for="aurthorname">Aurthor Name</label>
-                    <input type="text" id="aurthorname">
-                </div>
-                <div class="box">
-                    <label for="aurthorImage">Aurthor Image</label>
-                    <input type="file" id="aurthorImage" accept="image/png, image/jpeg">
+                    <div class="m-auto aboutImg"> Blog Image
+                        <img src=""  id="acceptImage">
+                        <iconify-icon icon="material-symbols:cloud-upload" class="aboutIcon" width="30" height="30" id="upload"></iconify-icon>
+
+                    </div>
+                    <input type="file" hidden id="picture" accept="image/png, image/jpeg" name="blogImage">
                 </div>
                 <div class="box">
                     <label for="fblink">FB Link</label>
-                    <input type="text" id="fblink">
+                    <input type="text" id="fblink" name="fbLink">
                 </div>
                 <div class="box">
                     <label for="instagramlink">Instagram Link</label>
-                    <input type="text" id="instagramlink">
+                    <input type="text" id="instagramlink" name="igLink">
                 </div>
                 <div class="box">
                     <label for="twitterlink">Twitter Link</label>
-                    <input type="text" id="twitterlink">
+                    <input type="text" id="twitterlink" name="twtLink">
                 </div>
             </div>
         </div>
         <div class="saveBtn ">
-            <button>Save Change <iconify-icon icon="material-symbols:save"></iconify-icon></button>
+            <button name="submit">Save Change <iconify-icon icon="material-symbols:save"></iconify-icon></button>
         </div>
     </form>
 
@@ -85,6 +98,14 @@ include "./resources/shared/dashboard.php";
     </div>
 
     <script src="./resources/js/dashboard_setting.js"></script>
+    <script>
+        $("#upload").click(() => {
+            $("#picture").click();
+            $("#picture").change(() => {
+                document.getElementById("acceptImage").src = window.URL.createObjectURL(event.target.files[0]);
+            })
+        })
+    </script>
 </body>
 
 </html>
