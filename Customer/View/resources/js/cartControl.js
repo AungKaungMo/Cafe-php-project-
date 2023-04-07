324f0e7f10ace0d502b7836184f40e5b4b853c4c
AungKaungMoe <moea74759@gmail.com>
2023-04-07 16:59:40 +0630

    d

--- Customer/View/resources/js/cartControl.js
+++ Customer/View/resources/js/cartControl.js
@@ -65,8 +65,17 @@ $(document).ready(() => {
         item.orderNote += " / " + noteValue;
       }
       $("#noteToOrder").val("");
+      setTimeout(function () {
+        location.reload();
+      }, 200);
     });
 
+    $(".closeReload").click(function () {
+      alert("hi");
+      setTimeout(function () {
+        location.reload();
+      }, 200);
+    });
     if (!found) {
       carts.push({
         id: itemId,
@@ -82,5 +91,5 @@ $(document).ready(() => {
     localStorage.setItem("cart", JSON.stringify(carts));
     $(".cartList").val(JSON.stringify(carts));
   });
-  console.log($(".cartList").val());
+  // console.log($(".cartList").val());
 });
