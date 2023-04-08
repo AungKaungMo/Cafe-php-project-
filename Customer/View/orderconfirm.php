b07ed2b6810fbb707105b19ea326994b3eae1398
AungKaungMoe <moea74759@gmail.com>
2023-04-08 22:48:24 +0630

    check

--- Customer/View/orderconfirm.php
+++ Customer/View/orderconfirm.php
@@ -69,7 +69,7 @@ include "../Controller/checkoutController.php";
                             Payment Type
                         </label><br><br>
                         <select id="payment" name="payment">
-                            <option value="1">KBZ</option>
+                            <!-- <option value="1">KBZ</option> -->
                             <option value="2">Wave</option>
                             <option value="3">COD</option>
                             <!-- <option value="4">Coin</option> -->
