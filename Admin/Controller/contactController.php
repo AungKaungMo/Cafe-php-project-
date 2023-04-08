398736e3a1ef0d386c9257a02193a14533622805
Lay <thwelay13@gmail.com>
2023-04-08 13:47:24 +0630

    pagination

--- Admin/Controller/contactController.php
+++ Admin/Controller/contactController.php
@@ -1,12 +1,39 @@
 <?php
+ini_set("display_errors", "1");
+
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = 1;
+}
+
+$rowLimit = 5;
+$pageStart = ($page - 1) * $rowLimit;
+$pageStart = ($pageStart < 0) ? 0 : $pageStart;
+
 include "../Model/dbConnection.php";
 
 $db = new DBConnection();
 $pdo = $db->connect();
+
+//all count
 $sql = $pdo->prepare(
     "
     SELECT * FROM m_sendcontactus WHERE del_flg = 0
     "
 );
 $sql->execute();
+$totalresult = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+
+//all list
+$sql = $pdo->prepare(
+    "
+    SELECT * FROM m_sendcontactus WHERE del_flg = 0
+    LIMIT $pageStart ,$rowLimit 
+    "
+);
+
+$sql->execute();
 $result = $sql->fetchAll(PDO::FETCH_ASSOC);
+$pageList = ceil(count($totalresult)) / $rowLimit;
